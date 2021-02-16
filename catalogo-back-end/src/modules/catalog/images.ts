612759d546ab08ebf9b1c0347dcd3c8bc1ffb0f8
HappyGick <egarciayustiz@gmail.com>
2021-02-16 13:30:36 -0400

    Imágenes del catálogo
    Añadida la capacidad de subir imágenes, y almacenarlas en la base de datos.

--- catalogo-back-end/src/modules/catalog/images.ts
+++ catalogo-back-end/src/modules/catalog/images.ts
@@ -1,11 +1,29 @@
-export function fetchImageById(id: string) {
+import fs from 'fs';
+import crypto from 'crypto';
 
+export function fetchImageById(id: string): Buffer {
+    return fs.readFileSync('./db/images/' + id + '.jpg');
 }
 
-export function uploadImage(image: Buffer) {
+export function uploadImage(image: Buffer): string {
+    let fileList = fs.readdirSync('./db/images');
+    let randomid : string;
+    
+    do {
+        randomid = crypto.randomBytes(32).toString('hex');
+    } while (fileList.includes(randomid + '.jpg'));
 
+    fs.writeFile('./db/images/' + randomid + '.jpg', image, (err) => {
+        if (err) throw err;
+    });
+
+    return randomid;
 }
 
 export function deleteImage(id: string) {
-    
+    fs.rmSync('./db/images/' + id + '.jpg');
+}
+
+export function isJPEG(image: Buffer): boolean {
+    return (image[0] === 0xff) && (image[1] === 0xd8) && (image[2] === 0xff);
 }
